c5cac876e8108589cd9573cf245c875fae0ac07c
Leonel Balsas <rldlab84@gmail.com>
2024-04-13 23:12:32 -0300

    setting up serve static files folder public and index.html

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.send('Hello World')
 })
+*/
+// Serve static files from the "public" directory
+app.use(express.static('public'));
 
 app.listen(3000)
